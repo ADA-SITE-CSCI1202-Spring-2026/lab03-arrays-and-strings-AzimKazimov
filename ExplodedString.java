df54bd1e26082c3532c702b6781859477b7a8fcb
AzimKazimov <akazimov23285@ada.edu.az>
2026-02-19 10:35:09 +0400

    Add files via upload

---
+++ ExplodedString.java
@@ -0,0 +1,23 @@
+import java.util.*;
+public class ExplodedString {
+   
+ public static String explode(String s) {
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = 0; i < s.length(); i++) {
+            sb.append(s.substring(0, i + 1));
+        }
+
+        return sb.toString();
+    }
+
+    public static void main(String[] args) {
+        String s1 = "Baku";
+        System.out.println(explode(s1));  
+    }
+    
+}
+    
+
+
+
