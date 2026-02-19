493ac8c7be2622a4de7fb3dde8322b0425274e05
AzimKazimov <akazimov23285@ada.edu.az>
2026-02-19 11:11:58 +0400

    Add files via upload

---
+++ MixedStrings.java
@@ -0,0 +1,38 @@
+import java.util.*;
+
+public class MixedStrings {
+    
+         public static String swap(String sentence) {
+        String[] words = sentence.split(" ");
+        StringBuilder result = new StringBuilder();
+
+        for (int i = 0; i < words.length; i++) {
+            String word = words[i];
+
+            if (word.length() > 1) {
+                char first = word.charAt(0);
+                char last = word.charAt(word.length() - 1);
+
+                String middle = word.substring(1, word.length() - 1);
+
+                word = last + middle + first;
+            }
+        
+
+            result.append(word);
+
+            if (i < words.length - 1) {
+                result.append(" ");
+            }
+        }
+
+        return result.toString();
+    }
+
+    public static void main(String[] args) {
+        String s1 = "This is PP2 Fall 2021";
+        System.out.println(swap(s1));
+        
+    }
+
+}
